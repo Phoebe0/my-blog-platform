5e64deffb6bad3b5b422eb078c0d48ca24a62db0
Tricia <7770188+chy99@user.noreply.gitee.com>
2024-12-31 18:04:02 +0800

    add: 初始化项目

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import Article from './views/Article'
+function App() {
+
+  return (
+      <Article />
+  )
+}
+
+export default App
---
+++ client/src/views/Article.tsx
@@ -0,0 +1,11 @@
+
+function Article() {
+
+    return (
+        <>
+            <h1>文章列表</h1>
+        </>
+    )
+}
+
+export default Article
