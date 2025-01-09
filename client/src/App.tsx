bc58af0d2ab768fa2e3a98b03d345dce860577ee
Tricia <7770188+chy99@user.noreply.gitee.com>
2025-01-09 17:13:08 +0800

    feat(服务端文件): 新建nest服务端项目
    [skip ci]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import Home from './views/Home/Home'
 // import DataZoom from './views/DataZoom.tsx'
 function App() {
 
-  return (
-      <Home />
-      // <DataZoom />
-  )
+    return (
+        <Home />
+        // <DataZoom />
+    )
 }
 
 export default App
