1bf7c97c37f93fbc0e90c7bca177f7366e7c6a42
Tricia <1552358695@qq.com>
2025-01-29 23:26:59 +0800

    feat(BlogNav): 导航栏开发
    完成导航栏样式开发以及移动端适配

--- client/src/components/BolgNav.tsx
+++ client/src/components/BolgNav.tsx
@@ -1,22 +1,48 @@
-// import React from 'react';
+import React, { useState } from 'react';
 
-const BolgNav = () => {
+const BlogNav: React.FC = () => {
+    // 下拉菜单状态
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
         <div className='w-full h-16 md:h-20 flex items-center justify-between'>
-        {/*logo*/}
-            <div className='flex items-center gap-2 text-l font-bold font-serif '>
-                <img src="../../public/logo.png" className='w-10 h-10' alt="logo"/>
-                <span className=''>Tricia's Blog</span>
+            {/* logo */}
+            <div className='flex items-center gap-2 text-l font-bold font-serif'>
+                <img src="../../public/logo.png" className='w-10 h-10 rounded-full' alt="logo" />
+                <span>Tricia's Blog</span>
             </div>
 
-        {/*mobile menu*/}
-            <div className='md:hidden'>M</div>
-        {/*desktop menu*/}
-            <div className='hidden md:flex'>D</div>
-
-
+            {/* 移动端按钮 */}
+            <div className='md:hidden'>
+                {/* 汉堡按钮 */}
+                <div className='cursor-pointer text-4xl' onClick={() => setOpen(prev => !prev)}>
+                    {open ? 'X' : '☰'}
+                </div>
+                {/* 移动端-菜单列表 */}
+                <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 
+                ${open ? '-right-0' : '-right-[100%]'} bg-white bg-opacity-50 gap-8 font-medium text-lg transition-all ease-in-out`}
+                >
+                    <a href="#">Home</a>
+                    <a href="#">Trending</a>
+                    <a href="#">Like</a>
+                    <a href="#">About</a>
+                    <a href="#">
+                        <button className='py-2 px-4 rounded-full bg-pink-600 text-white font-bold'>Login😺</button>
+                    </a>
+                </div>
+            </div>
+            {/* web端按钮 */}
+            <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
+                <a href="#">Home</a>
+                <a href="#">Trending</a>
+                <a href="#">Like</a>
+                <a href="#">About</a>
+                <a href="#">
+                    <button className='py-2 px-4 rounded-full bg-pink-600 text-white font-bold'>Login😺</button>
+                </a>
+            </div>
         </div>
     );
 };
 
-export default BolgNav;
+export default BlogNav;
